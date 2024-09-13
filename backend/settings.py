84ee74c941e44f89ae4993744e683c717d3eaa60
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-09-13 15:51:23 -0700

    adjust allowed hosts and requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.143.9.90"]
 
 
 # Application definition
